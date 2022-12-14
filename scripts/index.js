0880ee07ca54353acdd152e2d009c3366da4782c
t040496 <53284294+t040496@users.noreply.github.com>
2022-12-14 14:56:49 +0500

    style: отформатировать весь код в соотвтетсвии с код-стайлом Практикума

--- scripts/index.js
+++ scripts/index.js
@@ -5,14 +5,14 @@ const popup = document.querySelector('.popup');
 let form = document.querySelector('.popup__form');
 let profileTitle = document.querySelector('.profile__title');
 let profileSubtitle = document.querySelector('.profile__subtitle');
+const profileTitleTextContent = profileTitle.textContent;
+const profileSubtitleTextContent = profileSubtitle.textContent;
+const inputName = document.querySelector('#formName');
+const inputAbout = document.querySelector('#formSpecial');
 //открытие попап
 function openPopup(event) {
     event.preventDefault();
     popup.classList.add('popup__opened');
-    const profileTitleTextContent = profileTitle.textContent;
-    const profileSubtitleTextContent = profileSubtitle.textContent;
-    const inputName = document.querySelector('#formName');
-    const inputAbout = document.querySelector('#formSpecial');
     inputName.value = profileTitleTextContent;
     inputAbout.value = profileSubtitleTextContent;
 };
